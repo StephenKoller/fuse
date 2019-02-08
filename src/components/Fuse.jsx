b99314be3076d498afc40611d845d72af0841c7c
Stephen Koller <stephenkoller@gmail.com>
2019-02-08 16:19:08 -0500

    Setup emtpy Fuse component

---
+++ src/components/Fuse.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Fuse extends Component {
+  render() {
+    return (
+      <div className="fuse">
+        Fuse
+      </div>
+    )
+  }
+}
