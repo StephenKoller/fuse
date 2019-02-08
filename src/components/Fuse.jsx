e828f328698f7c7cf3884f6c29c80559025e1d80
Stephen Koller <stephenkoller@gmail.com>
2019-02-08 16:55:40 -0500

    WIP

--- src/components/Fuse.jsx
+++ src/components/Fuse.jsx
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const FuseContainer = styled.div`
+  height: 120px;
+`;
+
+const PlayButton = () => <button>Play</button>;
 
 export default class Fuse extends Component {
+  componentDidMount() {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const audioCtx = new AudioContext();
+    const audioElement = document.querySelector('audio');
+    const track = audioCtx.createMediaElementSource(audioElement);
+  }
+
   render() {
     return (
-      <div className="fuse">
-        Fuse
-      </div>
-    )
+      <FuseContainer>
+        <audio />
+        <PlayButton />
+      </FuseContainer>
+    );
   }
 }
